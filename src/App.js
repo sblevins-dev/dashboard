c6a25b62c48bb0ac08297a06dcbc434b05feeb12
Stephen Blevins <scblevins27@gmail.com>
2022-08-04 23:09:50 -0400

    set up navbar

--- src/App.js
+++ src/App.js
@@ -1,31 +1,19 @@
-import { Button, styled, Typography } from "@mui/material";
-import { Add, Settings } from "@mui/icons-material";
-
-const BlueButton = styled(Button)(({ theme }) => ({
-  backgroundColor: theme.palette.otherColor.main,
-        color:'#888',
-        margin:5,
-        "&:hover": {
-          backgroundColor:'lightblue'
-        },
-        "&:disabled":{
-          backgroundColor:'gray',
-          color:'white'
-        }
-}));
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, Container, Stack } from "@mui/material";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <div>
-      <Button variant="text">Text</Button>
-      <Button variant="contained" color="otherColor" startIcon={<Settings />}>Add new</Button>
-      <Button variant="outlined" startIcon={<Add />}>Outlined</Button>
-      <Typography variant="h1" component='h2'>
-        It uses h1 style
-      </Typography>
-      <BlueButton>My Button</BlueButton>
-      <BlueButton>My Button</BlueButton>
-    </div>
+    <Box>
+      <Navbar />
+      <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </Stack>
+    </Box>
   );
 }
 
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,94 @@
+import { Dashboard, Mail, Notifications } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  Menu,
+  MenuItem,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React, { useState } from "react";
+
+const StyledToolbar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  gap: "20px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+  },
+}));
+
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
+
+const Navbar = () => {
+  const [open, setOpen] = useState(false);
+  return (
+    <AppBar position="sticky">
+      <StyledToolbar>
+        <Typography variant="h6" sx={{ display: { xs: "none", sm: "block" } }}>
+          Dev
+        </Typography>
+        <Dashboard sx={{ display: { xs: "block", sm: "none" } }} />
+        <Search>
+          <InputBase placeholder="Search..." />
+        </Search>
+        <Icons>
+          <Badge badgeContent={4} color="error">
+            <Mail />
+          </Badge>
+          <Badge badgeContent={4} color="error">
+            <Notifications />
+          </Badge>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://miro.medium.com/max/909/1*_iikfMGYF3RH4OZ0yeQYnQ.png"
+            onClick={(e) => setOpen(true)}
+          />
+        </Icons>
+        <UserBox onClick={(e) => setOpen(true)}>
+          <Avatar
+            sx={{ width: 30, height: 30 }}
+            src="https://miro.medium.com/max/909/1*_iikfMGYF3RH4OZ0yeQYnQ.png"
+          />
+          <Typography variant="span">Jan</Typography>
+        </UserBox>
+      </StyledToolbar>
+      <Menu
+        id="demo-positioned-menu"
+        open={open}
+        onClose={(e) => setOpen(false)}
+        anchorOrigin={{ vertical: "top", horizontal: "right" }}
+        transformOrigin={{ vertical: "top", horizontal: "right" }}
+      >
+        <MenuItem>Profile</MenuItem>
+        <MenuItem>My Account</MenuItem>
+        <MenuItem>Logout</MenuItem>
+      </Menu>
+    </AppBar>
+  );
+};
+
+export default Navbar;
